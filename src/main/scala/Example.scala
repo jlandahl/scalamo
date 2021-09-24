115228c7309b8bafcd685bd04137e925491e588d
John Landahl <john@landahl.org>
2021-09-24 12:52:42 -0700

    Added code example

---
+++ src/main/scala/Example.scala
@@ -0,0 +1,16 @@
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.document.DynamoDB
+import java.time.Instant
+import scalamo._
+
+case class User(userId: String, name: String, lastLogin: Instant)
+
+object Example extends App {
+  val dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient())
+  val table = dynamoDB.getTable("users")
+  val maybeUser = table.get[User]("userId", "12345") // returns scalamo.Validated[User], which is cats.data.ValidatedNel[Throwable, User]
+
+  maybeUser.foreach { user =>
+    table.put(user.copy(lastLogin = Instant.now()))
+  }
+}
